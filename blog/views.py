eb8d587e313692d9ffa51ac0ceaac9f19753b68f
Aeius <psb6604@gmail.com>
2022-06-16 22:46:36 +0900

    article

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from blog.models import Article
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import permissions
 
 # Create your views here.
+
+
+class ArticleView(APIView):
+    permissions_classes = [permissions.AllowAny]
+
+    def get(self, request):
+        user = request.user
+        articles = Article.objects.filter(author=user)
+        return Response({"titles":articles})
