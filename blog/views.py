5a6ec4b04277d6c9e5a89f649cfee84ff056510e
Aeius <psb6604@gmail.com>
2022-06-20 14:05:20 +0900

    DRF insert article with vaildations #1

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
+from tkinter import N
+from tkinter.messagebox import NO
 from blog.models import Article
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -17,3 +19,20 @@ class ArticleView(APIView):
             title_list.append(article.title)
 
         return Response(title_list)
+
+    def post(self, request):
+        title = request.data.get("title", "")
+        category = request.data.get("category", "")
+        content = request.data.get("content", "")
+        
+        if len(title) < 5:
+            return Response({'error':'제목은 5자 이상입니다.'})
+        if len(content) < 20:
+            return Response({'error':'내용은 20자 이상입니다.'})
+        if not category:
+            return Response({'error':'카테고리를 지정해주세요.'})
+        
+        return Response({'success': '게시글 작성완료!',
+                         'title': title,
+                         'content': content,
+                         'category': category})
